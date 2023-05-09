6c08491404a7cf1bec5be6f62550f0bafa1f3051
HarshKumar Jha <jhaharsh878@gmail.com>
2023-05-09 14:30:33 +0530

    added wrong commit

--- src/index.js
+++ src/index.js
@@ -3,219 +3,221 @@ import net from "net";
 import statusCodes from "./utils/statusCodes.js";
 
 class RestWave extends Methods {
-	#server;
-	#request;
-	#response;
-	#socket;
-	#data;
-	#contentLength;
-   #header;
+  #server;
+  #request;
+  #response;
+  #socket;
+  #data;
+  #contentLength;
 
-	constructor() {
-		super();
-		this.#setResponseType();
-		this.#request = {
-			method: "",
-			url: "",
-			body: "",
-			headers: {},
-		};
-      this.#response.setHeader = (key,value)=>{
-         this.#header[key] = value;
+  constructor() {
+    super();
+    this.#setResponseType();
+    this.#request = {
+      method: "",
+      url: "",
+      data: "",
+      headers: {},
+    };
+    this.#createServer();
+  }
+
+  #createServer() {
+    this.#server = net.createServer((socket) => {
+      let body = "";
+      this.#contentLength = 0;
+      this.#data = "";
+      socket.on("data", (data) => {
+        this.#socket = socket;
+        body = data.toString("utf-8");
+        const lines = body.split("\r\n");
+        // console.log(lines);
+        if (lines.length > 1) {
+          const contentTypeHeader = lines.find((line) =>
+            line.startsWith("Content-Type:")
+          );
+          if (
+            contentTypeHeader &&
+            contentTypeHeader.includes("application/json")
+          ) {
+            const contentIndex = body.indexOf("\r\n\r\n") + 4;
+            this.#request.data = JSON.parse(body.slice(contentIndex));
+          }
+        }
+
+        // Attach headers to req
+        for (let i = 0; i < lines.length; i++) {
+          const [key, value] = lines[i].split(":");
+          if (!key || !value) continue;
+          this.#request.headers[key] = value;
+        }
+
+        this.#request.method = body.split(" ")[0];
+        this.#request.url = body.split(" ")[1];
+
+        if (this.#request.method === "OPTIONS") {
+          this.#socket.write(
+            `HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\nAccess-Control-Allow-Methods: GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS\r\nAccess-Control-Allow-Origin: *`
+          );
+          this.#socket.end();
+          return;
+        }
+        this.#setResponseType();
+        this.#handleRequests();
+      });
+    });
+  }
+
+  #handleRequests() {
+    this.#extractQueryParameters();
+    let i = 0;
+    const next = () => {
+      const currentMiddleware = super.getMiddlewares()[i];
+
+      i++;
+      if (typeof currentMiddleware === "function") {
+        currentMiddleware(this.#request, this.#response, next);
+      } else if (i <= super.getMiddlewares().length) {
+        const method = this.#request.method;
+        if (method === "GET" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "PATCH" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "POST" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "DELETE" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (currentMiddleware.method === "ANY") {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else {
+          if (i <= super.getMiddlewares().length) {
+            next();
+          } else {
+            throw new Error("Requested endpoint not handled");
+          }
+        }
+      } else {
+        throw new Error("Requested endpoint not handled");
       }
-		this.#createServer();
-	}
+    };
+    next();
+  }
 
-	#createServer() {
-		this.#server = net.createServer((socket) => {
-			let body = "";
-         this.#response.setHeader("content-type","application/json");
+  #handleMethodRequests(currentMiddleware, next) {
+    if (currentMiddleware.hasOwnProperty("params")) {
+      this.#request.params = {};
+      const rUrl = this.#request.url.split("/");
+      const cUrl = currentMiddleware.route.split("/");
+      let currentParam = 0;
+      if (rUrl.length !== cUrl.length) return false;
+      for (let i = 0; i < rUrl.length; i++) {
+        if (
+          cUrl[i].startsWith(":") &&
+          currentParam < currentMiddleware.params.length &&
+          rUrl[i]
+        ) {
+          this.#request.params[
+            Object.keys(currentMiddleware.params[currentParam])[0]
+          ] = rUrl[i];
+          currentParam++;
+        } else {
+          if (cUrl[i] !== rUrl[i]) {
+            this.#request.params = {};
+            return false;
+          }
+        }
+      }
+      currentMiddleware.cb(this.#request, this.#response, next);
+      return true;
+    } else if (currentMiddleware.route === this.#request.url) {
+      currentMiddleware.cb(this.#request, this.#response, next);
+      return true;
+    }
+  }
 
-			this.#contentLength = 0;
-			this.#data = "";
-			socket.on("data", (data) => {
-				this.#socket = socket;
-				body = data.toString("utf-8");
-				const lines = body.split("\r\n");
-				// console.log(lines);
-				if (lines.length > 1) {
-					const contentTypeHeader = lines.find((line) =>
-						line.startsWith("Content-Type:")
-					);
-					if (
-						contentTypeHeader &&
-						contentTypeHeader.includes("application/json")
-					) {
-						const contentIndex = body.indexOf("\r\n\r\n") + 4;
-						this.#request.body = JSON.parse(body.slice(contentIndex));
-					}
-				}
+  #extractQueryParameters() {
+    const queryParameters = {};
+    const [urlString, queryString] = this.#request.url.split("?");
+    if (!queryString) return;
+    queryString.split("&").forEach((query) => {
+      const [key, value] = query.split("=");
+      queryParameters[key] = value;
+    });
+    this.#request.query = queryParameters;
+    this.#request.url = urlString;
+  }
 
+  #setResponseType() {
+    const writeResponse = (arg, contentType) => {
+      this.#contentLength += arg.length;
+      const content = `${this.#data}\r\n\r\n${arg}`;
+      this.#data += content;
+      return `HTTP/1.1 ${this.#response.statusCode} ${
+        statusCodes[this.#response.statusCode]
+      }\r\nAccess-Control-Allow-Methods: GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: ${contentType}\r\nContent-Length: ${this.#contentLength}${content}`;
+    };
 
+    this.#response = {
+      statusCode: 200,
+      json: (arg, sc) => {
+        if (sc) this.#response.statusCode = sc;
+        if (arg) {
+          arg = JSON.stringify(arg);
+          const sike = writeResponse(arg, 'application/json');
+          this.#socket.write(sike, "utf-8", () => {
+            this.#socket.end();
+          });
+        } else {
+          this.#socket.write(writeResponse(""), "utf-8", () => {
+            this.#socket.end();
+          });
+        }
+      },
+      send: async (arg, sc) => {
+        if (sc) this.#response.statusCode = sc;
+        if (arg) {
+          const buffer = fs.createReadStream('<h1>Hello, World!</h1>');
+          const contentType = await fileTypeFromBuffer(buffer)
+         console.log(contentType);
+          const sike = writeResponse(arg, contentType);
+          this.#socket.write(sike, "utf-8", () => {
+            this.#socket.end();
+          });
+        } else {
+          this.#socket.write(writeResponse(""), "utf-8", () => {
+            this.#socket.end();
+          });
+        }
+      },
+    };
+  }
 
-				// Attach headers to req
-				for (let i = 0; i < lines.length; i++) {
-					const [key, value] = lines[i].split(":");
-					if (!key || !value) continue;
-					this.#request.headers[key] = value;
-				}
-
-				this.#request.method = body.split(" ")[0];
-				this.#request.url = body.split(" ")[1];
-
-				if (this.#request.method === "OPTIONS") {
-					this.#socket.write(
-						`HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\nAccess-Control-Allow-Methods: GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS\r\nAccess-Control-Allow-Origin: *`
-					);
-					this.#socket.end();
-					return;
-				}
-				this.#setResponseType();
-				this.#handleRequests();
-			});
-		});
-	}
-
-
-	#handleRequests() {
-		this.#extractQueryParameters();
-		let i = 0;
-		const next = () => {
-			const currentMiddleware = super.getMiddlewares()[i];
-
-			i++;
-			if (typeof currentMiddleware === "function") {
-				currentMiddleware(this.#request, this.#response, next);
-			} else if (i <= super.getMiddlewares().length) {
-				const method = this.#request.method;
-				if (method === "GET" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "PATCH" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "POST" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "DELETE" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (currentMiddleware.method === "ANY") {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else {
-					if (i <= super.getMiddlewares().length) {
-						next();
-					} else {
-						throw new Error("Requested endpoint not handled");
-					}
-				}
-			} else {
-				throw new Error("Requested endpoint not handled");
-			}
-		};
-		next();
-	}
-
-	#handleMethodRequests(currentMiddleware, next) {
-		if (currentMiddleware.hasOwnProperty("params")) {
-			this.#request.params = {};
-			const rUrl = this.#request.url.split("/");
-			const cUrl = currentMiddleware.route.split("/");
-			let currentParam = 0;
-			if (rUrl.length !== cUrl.length) return false;
-			for (let i = 0; i < rUrl.length; i++) {
-				if (
-					cUrl[i].startsWith(":") &&
-					currentParam < currentMiddleware.params.length &&
-					rUrl[i]
-				) {
-					this.#request.params[
-						Object.keys(currentMiddleware.params[currentParam])[0]
-					] = rUrl[i];
-					currentParam++;
-				} else {
-					if (cUrl[i] !== rUrl[i]) {
-						this.#request.params = {};
-						return false;
-					}
-				}
-			}
-			currentMiddleware.cb(this.#request, this.#response, next);
-			return true;
-		} else if (currentMiddleware.route === this.#request.url) {
-			currentMiddleware.cb(this.#request, this.#response, next);
-			return true;
-		}
-	}
-
-	#extractQueryParameters() {
-		const queryParameters = {};
-		const [urlString, queryString] = this.#request.url.split("?");
-		if (!queryString) return;
-		queryString.split("&").forEach((query) => {
-			const [key, value] = query.split("=");
-			queryParameters[key] = value;
-		});
-		this.#request.query = queryParameters;
-		this.#request.url = urlString;
-	}
-
-	#setResponseType() {
-		const writeResponse = (arg) => {
-			this.#contentLength += arg.length;
-         this.#response.setHeader("Content-Length",`${this.#contentLength}`);
-			const content = `${this.#data}\r\n\r\n${arg}`;
-			this.#data += content;
-			return `HTTP/1.1 ${this.#response.statusCode} ${
-				statusCodes[this.#response.statusCode]
-			}\r\nAccess-Control-Allow-Methods: GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: application/json\r\nContent-Length: ${
-				this.#contentLength
-			}${content}`;
-		};
-
-		this.#response = {
-			statusCode: 200,
-			json: (arg, sc) => {
-				if (sc) this.#response.statusCode = sc;
-				if (arg) {
-					arg = JSON.stringify(arg);
-					const sike = writeResponse(arg);
-					this.#socket.write(sike, "utf-8", () => {
-						this.#socket.end();
-					});
-				} else {
-					this.#socket.write(writeResponse(""), "utf-8", () => {
-						this.#socket.end();
-					});
-				}
-			},
-		};
-	}
-
-	listen(...args) {
-		// if (args[0] && typeof args[0] !== "number")
-		// 	throw new Error("PORT must be a number");
-		let port ;
-		let host;
-		let cb;
-		if (args.length === 1) {
-			port = Number(args[0]);
-		} else if (args.length === 2) {
-			port = Number(args[0]);
-			cb = args[1];
-		} else {
-			port = Number(args[0]);
-			host = args[1];
-			cb = args[2];
-		}
-		this.#server.listen(port, host, cb);
-	}
+  listen(...args) {
+    let port =Number(args[0]) || 3000;
+    let host;
+    let cb;
+    if (args.length === 1) {
+      port = args[0];
+    } else if (args.length === 2) {
+      port = args[0];
+      cb = args[1];
+    } else {
+      port = args[0];
+      host = args[1];
+      cb = args[2];
+    }
+    this.#server.listen(port, host, cb);
+  }
 }
 
 export default RestWave;
--- src/test.js
+++ src/test.js
@@ -1,14 +1,13 @@
 import RestWave from "./index.js";
-import cors from 'cors'
 const app = new RestWave();
 app.printMiddlewares();
-app.use(cors());
 app.get("/", (req, res) => {
-	res.json({ name: " adarsh", last: "shahi", age: 21 }, 400);
+	res.send({ name: " adarsh", last: "shahi", age: 21 }, 500);
 });
 app.post("/", (req, res) => {
    console.log(req.body);
-	res.json({ name: " adarsh", last: "shahi", age: 21 }, 500);
+   const bufferData = Buffer.from({ name: " adarsh", last: "shahi", age: 21 });
+	res.send(bufferData, 500);
 });
 app.patch("/", (req, res) => {
 	res.json({ name: " adarsh", last: "shahi", age: 21 }, 500);
