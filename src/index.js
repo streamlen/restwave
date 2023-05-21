49c534f65634c8f534d6521e0311c74c648658db
HarshKumar Jha <jhaharsh878@gmail.com>
2023-05-22 00:08:05 +0530

    added error handling functionality

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { getTextContentType, getFileContentType } from "./utils/contentType.js";
 import fs from "fs";
 
 class RestWave extends Methods {
-	#server;
-	#request;
-	#response;
-	#socket;
-	#data;
-	#contentLength;
-	#responseHeaders = {};
+  #server;
+  #request;
+  #response;
+  #socket;
+  #data;
+  #contentLength;
+  #responseHeaders = {};
 
   constructor() {
     super();
@@ -23,6 +23,18 @@ class RestWave extends Methods {
       headers: {},
     };
     this.#createServer();
+    this.#unhandledErrors();
+  }
+
+  #unhandledErrors(){
+   process.on('unhandledRejection',(err,req,res,next)=>{
+      console.error('unhandled Rejection', err);
+      process.exit(1);
+    })
+    process.on('uncaughtException',async (err,req,res,next)=>{
+      console.error('unhandled Exception', err);
+      process.exit(1);
+    })
   }
 
   #createServer() {
@@ -57,71 +69,78 @@ class RestWave extends Methods {
         this.#request.method = body.split(" ")[0];
         this.#request.url = body.split(" ")[1];
 
-				if (this.#request.method === "OPTIONS") {
-					this.#socket.write(
-						`HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\nAccess-Control-Allow-Headers: *\r\nAccess-Control-Allow-Methods: GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS\r\nAccess-Control-Allow-Origin: *`
-					);
-					this.#socket.end();
-					return;
-				}
-				this.#setResponseType();
-				this.#handleRequests();
-			});
+        if (this.#request.method === "OPTIONS") {
+          this.#socket.write(
+            `HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\nAccess-Control-Allow-Headers: *\r\nAccess-Control-Allow-Methods: GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS\r\nAccess-Control-Allow-Origin: *`
+          );
+          this.#socket.end();
+          return;
+        }
+        this.#setResponseType();
+        this.#handleRequests();
+      });
 
-			socket.on("error", (err) => {
-				console.log(err);
-			});
-		});
-	}
+      socket.on("error", (err) => {
+        console.log(err);
+      });
+    });
+  }
 
   #handleRequests() {
     this.#extractQueryParameters();
     let i = 0;
-    const next = () => {
+    const next = (err) => {
       const currentMiddleware = super.getMiddlewares()[i];
-
-			i++;
-			if (typeof currentMiddleware === "function") {
-				currentMiddleware(this.#request, this.#response, next);
-			} else if (i <= super.getMiddlewares().length) {
-				const method = this.#request.method;
-				if (method === "GET" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "PATCH" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "POST" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "DELETE" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (method === "PUT" && currentMiddleware.method === method) {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else if (currentMiddleware.method === "ANY") {
-					if (!this.#handleMethodRequests(currentMiddleware, next)) {
-						next();
-					}
-				} else {
-					if (i <= super.getMiddlewares().length) {
-						next();
-					} else {
-						// throw new Error("Requested endpoint not handled");
-					}
-				}
-			} else {
-				// throw new Error("Requested endpoint not handled");
-			}
-		};
-		next();
-	}
+      i++;
+      if (i <= currentMiddleware.length && err instanceof Error) {
+         const argLength = currentMiddleware.length;
+        if(argLength===4){
+         currentMiddleware(err,this.#request,this.#response,next);
+        }
+        else{
+         next(err);
+        }
+      } else if (i <= currentMiddleware.length && typeof currentMiddleware === "function") {
+        currentMiddleware(this.#request, this.#response, next);
+      } else if (i <= currentMiddleware.length) {
+        const method = this.#request.method;
+        if (method === "GET" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "PATCH" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "POST" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "DELETE" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (method === "PUT" && currentMiddleware.method === method) {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else if (currentMiddleware.method === "ANY") {
+          if (!this.#handleMethodRequests(currentMiddleware, next)) {
+            next();
+          }
+        } else {
+          if (i <= super.getMiddlewares().length) {
+            next();
+          } else {
+            // throw new Error("Requested endpoint not handled");
+          }
+        }
+      } else {
+        // throw new Error("Requested endpoint not handled");
+      }
+    };
+    next();
+  }
 
   #handleMethodRequests(currentMiddleware, next) {
     if (currentMiddleware.hasOwnProperty("params")) {
@@ -167,98 +186,98 @@ class RestWave extends Methods {
     this.#request.url = urlString;
   }
 
-	#setResponseType() {
-		const writeResponse = (arg) => {
-			this.#contentLength += arg.length;
-			const content = `\r\n\r\n${arg}`;
-			return `HTTP/1.1 ${this.#response.statusCode} ${
-				statusCodes[this.#response.statusCode]
-			}\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: ${
-				this.#responseHeaders["Content-Type"]
-			}\r\nContent-Length: ${this.#contentLength}${content}`;
-		};
+  #setResponseType() {
+    const writeResponse = (arg) => {
+      this.#contentLength += arg.length;
+      const content = `\r\n\r\n${arg}`;
+      return `HTTP/1.1 ${this.#response.statusCode} ${
+        statusCodes[this.#response.statusCode]
+      }\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: ${
+        this.#responseHeaders["Content-Type"]
+      }\r\nContent-Length: ${this.#contentLength}${content}`;
+    };
 
-		this.#response = {
-			statusCode: 200,
-			setHeaders: (obj) => {
-				this.#responseHeaders = { ...this.#responseHeaders, ...obj };
-			},
-			json: (arg, sc) => {
-				if (sc) this.#response.statusCode = sc;
-				if (arg) {
-					arg = JSON.stringify(arg);
-					const sike = writeResponse(arg);
-					this.#socket.write(sike, "utf-8", () => {
-						this.#socket.end();
-					});
-				} else {
-					this.#socket.write(writeResponse(""), "utf-8", () => {
-						this.#socket.end();
-					});
-				}
-			},
-			send: (arg = "", sc) => {
-				if (sc) this.#response.statusCode = sc;
-				if (arg) {
-					if (!this.#responseHeaders.hasOwnProperty("Content-Type")) {
-						const type = getTextContentType(arg);
-						if (type === "application/json") {
-							arg = JSON.stringify(arg);
-						}
-						this.#response.setHeaders({
-							"Content-Type": type,
-						});
-					}
-				}
-				const sike = writeResponse(arg);
-				this.#socket.write(sike, "utf-8", () => {
-					this.#socket.end();
-				});
-			},
+    this.#response = {
+      statusCode: 200,
+      setHeaders: (obj) => {
+        this.#responseHeaders = { ...this.#responseHeaders, ...obj };
+      },
+      json: (arg, sc) => {
+        if (sc) this.#response.statusCode = sc;
+        if (arg) {
+          arg = JSON.stringify(arg);
+          const sike = writeResponse(arg);
+          this.#socket.write(sike, "utf-8", () => {
+            this.#socket.end();
+          });
+        } else {
+          this.#socket.write(writeResponse(""), "utf-8", () => {
+            this.#socket.end();
+          });
+        }
+      },
+      send: (arg = "", sc) => {
+        if (sc) this.#response.statusCode = sc;
+        if (arg) {
+          if (!this.#responseHeaders.hasOwnProperty("Content-Type")) {
+            const type = getTextContentType(arg);
+            if (type === "application/json") {
+              arg = JSON.stringify(arg);
+            }
+            this.#response.setHeaders({
+              "Content-Type": type,
+            });
+          }
+        }
+        const sike = writeResponse(arg);
+        this.#socket.write(sike, "utf-8", () => {
+          this.#socket.end();
+        });
+      },
 
-			sendFile: (path) => {
-				if (!path) throw new Error("Path name expected.");
-				const file = fs.readFileSync(path);
-				if (!this.#responseHeaders.hasOwnProperty("Content-Type")) {
-					const type = getFileContentType(path);
-					this.#response.setHeaders({
-						"Content-Type": type,
-					});
-				}
-				// Prepare HTTP headers
-				const headers = [
-					`HTTP/1.1 ${this.#response.statusCode} ${
-						statusCodes[this.#response.statusCode]
-					}`,
-					`Content-Type: ${getFileContentType(path)}`,
-					`Content-Length: ${file.length}`,
-					"Access-Control-Allow-Origin: *",
-					"Connection: close",
-					"\r\n",
-				].join("\r\n");
+      sendFile: (path) => {
+        if (!path) throw new Error("Path name expected.");
+        const file = fs.readFileSync(path);
+        if (!this.#responseHeaders.hasOwnProperty("Content-Type")) {
+          const type = getFileContentType(path);
+          this.#response.setHeaders({
+            "Content-Type": type,
+          });
+        }
+        // Prepare HTTP headers
+        const headers = [
+          `HTTP/1.1 ${this.#response.statusCode} ${
+            statusCodes[this.#response.statusCode]
+          }`,
+          `Content-Type: ${getFileContentType(path)}`,
+          `Content-Length: ${file.length}`,
+          "Access-Control-Allow-Origin: *",
+          "Connection: close",
+          "\r\n",
+        ].join("\r\n");
 
-				// Combine headers and video file data
-				const httpResponse = Buffer.concat([Buffer.from(headers), file]);
+        // Combine headers and video file data
+        const httpResponse = Buffer.concat([Buffer.from(headers), file]);
 
-				// Step 4: Send the HTTP response to the client
-				// this.#socket.write(writeResponse(file));
-				this.#socket.write(httpResponse);
-				this.#socket.end();
+        // Step 4: Send the HTTP response to the client
+        // this.#socket.write(writeResponse(file));
+        this.#socket.write(httpResponse);
+        this.#socket.end();
 
-				// const fileReadHandler = await fs.open(path);
-				// const stream = fileReadHandler.createReadStream();
-				// this.#response.setHeaders({ "Content-Type": "video/mp4" });
-				// this.#socket.write(
-				// 	`HTTP/1.1 206 ${
-				// 		statusCodes["206"]
-				// 	}\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: ${
-				// 		this.#responseHeaders["Content-Type"]
-				// 	}`
-				// );
-				// stream.pipe(this.#socket.write);
-			},
-		};
-	}
+        // const fileReadHandler = await fs.open(path);
+        // const stream = fileReadHandler.createReadStream();
+        // this.#response.setHeaders({ "Content-Type": "video/mp4" });
+        // this.#socket.write(
+        // 	`HTTP/1.1 206 ${
+        // 		statusCodes["206"]
+        // 	}\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: ${
+        // 		this.#responseHeaders["Content-Type"]
+        // 	}`
+        // );
+        // stream.pipe(this.#socket.write);
+      },
+    };
+  }
 
   listen(...args) {
     let port = Number(args[0]) || 3000;
